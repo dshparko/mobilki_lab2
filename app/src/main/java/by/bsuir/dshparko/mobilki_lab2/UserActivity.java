769d090db488db606517d5836c4488c4ce38d662
Lenovo <d.shparko@mail.ru>
2022-04-09 21:37:57 +0300

    add map

--- app/src/main/java/by/bsuir/dshparko/mobilki_lab2/UserActivity.java
+++ app/src/main/java/by/bsuir/dshparko/mobilki_lab2/UserActivity.java
@@ -1,8 +1,12 @@
 package by.bsuir.dshparko.mobilki_lab2;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
@@ -10,11 +14,23 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -22,6 +38,8 @@ public class UserActivity extends AppCompatActivity {
     TextView name, email;
     ImageView userPhoto;
 
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,23 +51,74 @@ public class UserActivity extends AppCompatActivity {
             Uri personPhoto = acct.getPhotoUrl();
             name = findViewById(R.id.userName);
             userPhoto = findViewById(R.id.userPhoto);
+
             Glide.with(getApplicationContext())
                   .load(personPhoto)
                     .into(userPhoto);
 
-            System.out.println(personPhoto);
             email = findViewById(R.id.post);
             name.setText(personName);
             email.setText(personEmail);
+
+
+            supportMapFragment =(SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.maps);
+
+
+            client = LocationServices.getFusedLocationProviderClient(this);
+            if(ActivityCompat.checkSelfPermission(UserActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }else{
+                ActivityCompat.requestPermissions(UserActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+            }
+
         } else {
             Intent intent1 = new Intent(this, SignInActivity.class);
             startActivity(intent1);
         }
     }
 
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+       if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            }
+        }
+    }
+
+    public void getCurrentLocation() {
+        @SuppressLint("MissingPermission")
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("I'm right here!");
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+                            googleMap.addMarker(options).showInfoWindow();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void signOut(View view){
+        Intent intent1 = new Intent(this, SignOutActivity.class);
+        startActivity(intent1);
+    }
+
     public void showInformationAboutCreator(View view) {
 
-        String info = "lab2\nCreator: Darya Shparko ";
+        String info = "lab2\nCreator: Darya Shparko, 951005 ";
 
         final AlertDialog aboutDialog = new AlertDialog.Builder(
                 UserActivity.this).setMessage(info)
